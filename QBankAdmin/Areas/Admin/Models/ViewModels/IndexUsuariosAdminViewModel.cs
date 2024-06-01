132e0496c2a1a552155c210543b932512e21229e
RodrigoTLZ <201G0246@rcarbonifera.tecnm.mx>
2024-06-01 13:14:16 -0600

    Vm index usuarios

---
+++ QBankAdmin/Areas/Admin/Models/ViewModels/IndexUsuariosAdminViewModel.cs
@@ -0,0 +1,9 @@
+﻿using QBankAdmin.Models.Dtos;
+
+namespace QBankAdmin.Areas.Admin.Models.ViewModels
+{
+    public class IndexUsuariosAdminViewModel
+    {
+        public IEnumerable<UsuarioDTO> Usuarios { get; set; }
+    }
+}
