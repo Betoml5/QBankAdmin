2cfaf42f628bf5c4dd08ad23c576f949f6f54d20
RodrigoTLZ <201G0246@rcarbonifera.tecnm.mx>
2024-05-31 18:58:08 -0600

    cambios crud cajas

---
+++ QBankAdmin/Areas/Admin/Models/ViewModels/IndexAdminViewModel.cs
@@ -0,0 +1,10 @@
+﻿using QBankAdmin.Models.Dtos;
+
+namespace QBankAdmin.Areas.Admin.Models.ViewModels
+{
+    public class IndexAdminViewModel
+    {
+        public IEnumerable<CajaDTO>? Cajas { get; set; }
+
+    }
+}
