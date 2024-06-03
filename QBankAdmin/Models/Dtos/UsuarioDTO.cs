c87596eceaedcbb7496e006a8e44944ff0f9cb93
RodrigoTLZ <201G0246@rcarbonifera.tecnm.mx>
2024-06-03 16:44:33 -0600

    usuario dto modificado

--- QBankAdmin/Models/Dtos/UsuarioDTO.cs
+++ QBankAdmin/Models/Dtos/UsuarioDTO.cs
@@ -6,7 +6,7 @@
         public int Id { get; set; }
 
         public string NombreUsuario { get; set; } = null!;
-
+        public string CorreoElectronico { get; set; } = null!;
         public string Contrasena { get; set; } = null!;
 
         public int RolId { get; set; }
