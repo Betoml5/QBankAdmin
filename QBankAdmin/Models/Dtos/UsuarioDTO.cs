710253bee4d845b4d01494f089740adc98c2ea82
Betoml5 <betoml5@hotmail.com>
2024-06-03 01:14:18 -0600

    Agregue estilos a operador y usuarios

--- QBankAdmin/Models/Dtos/UsuarioDTO.cs
+++ QBankAdmin/Models/Dtos/UsuarioDTO.cs
@@ -7,6 +7,8 @@
 
         public string NombreUsuario { get; set; } = null!;
 
+        public string Contrasena { get; set; } = null!;
+
         public int RolId { get; set; }
 
         public string Rol { get; set; } = null!;
