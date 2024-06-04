af83db3c91ab18e0801b89a9686dcdd8e0e32438
Betoml5 <betoml5@hotmail.com>
2024-06-04 17:35:24 -0600

    separe js a archivo separado

---
+++ QBankAdmin/wwwroot/operador.js
@@ -0,0 +1,168 @@
+﻿const urlLocal = "https://localhost:5002/turno";
+const url = "https://qbank.websitos256.com/turno";
+const btnNext = document.getElementById("btnNext");
+const $turnoActual = document.querySelector(".container__operador-turnos-actual p");
+const $btnsAtender = document.querySelectorAll(".container__operador-cajas-item button");
+const $containerTurnosControl = document.querySelector(".container__operador-turnos-control");
+const $containerCajasControl = document.querySelector(".container__operador-cajas");
+const $caja = document.querySelector(".container__operador-caja");
+const $btnSkipTurno = document.querySelector("#btnSkip");
+const $btnEndTurno = document.getElementById("btnEndTurno");
+
+const cajaId = localStorage.getItem("cajaId")
+const cajaNumero = localStorage.getItem("cajaNumero");
+
+
+const connection = new signalR.HubConnectionBuilder()
+    .withUrl(urlLocal, {
+        skipNegotiation: true,
+        transport: signalR.HttpTransportType.WebSockets,
+    })
+    .configureLogging(signalR.LogLevel.Information)
+    .build();
+
+
+$btnEndTurno.addEventListener("click", endTurno);
+
+$btnsAtender.forEach((btn) => {
+    btn.addEventListener("click", atender);
+});
+
+async function atender(e) {
+
+    const dataset = e.target.parentElement.querySelector("p").dataset;
+    console.log(dataset)
+    localStorage.setItem("cajaId", dataset.cajaId);
+    localStorage.setItem("cajaNumero", dataset.cajaNumero);
+
+    $caja.style.display = "block";
+    $caja.textContent = `Caja: ${dataset.cajaNumero}`;
+    $containerCajasControl.style.display = "none";
+    $containerTurnosControl.style.display = "block";
+
+    await connection.invoke("SetActiveCaja", +dataset.cajaId);
+
+
+}
+
+async function start() {
+    try {
+        await connection.start();
+        console.log("SignalR Connected.");
+    } catch (err) {
+        console.log(err);
+        setTimeout(start, 5000);
+    }
+};
+
+connection.onclose(async () => {
+    await start();
+});
+
+connection.on("AddToQueue", (turno) => {
+    const turnosContainer = document.querySelector(".container__operador-turnos");
+    const $div = document.createElement("div");
+    $div.classList.add("container__operador-turnos-item");
+    const pTurno = document.createElement("p");
+    const pEstado = document.createElement("p");
+    pTurno.textContent = `Turno: ${turno.codigoTurno}`;
+    pEstado.textContent = `${turno.estado}`;
+    $div.appendChild(pTurno);
+    $div.appendChild(pEstado);
+    turnosContainer.appendChild($div);
+});
+
+connection.on("SetCurrentTurn", (turno, cajaId) => {
+
+    if (turno, cajaId) {
+        const turnos = document.querySelectorAll(".container__operador-turnos-item");
+        $turnoActual.textContent = `Turno: ${turno}`;
+        $turnoActual.dataset.turno = turno;
+        turnos.forEach((item) => {
+            if (item.textContent.includes(turno)) {
+                item.remove();
+            }
+        });
+    }
+
+
+});
+
+connection.on("SkipTurn", (turno, siguienteTurno) => {
+
+
+    if (turno, siguienteTurno) {
+        $turnoActual.textContent = `Turno: ${siguienteTurno.codigoTurno}`;
+        $turnoActual.dataset.turno = siguienteTurno.codigoTurno;
+    }
+
+
+});
+
+btnNext.addEventListener("click", async () => {
+    const cajaId = localStorage.getItem("cajaId");
+    const cajaNumero = localStorage.getItem("cajaNumero");
+    if (cajaId) {
+        console.log(cajaId)
+        btnNext.disabled = true;
+        btnNext.style.cursor = "not-allowed";
+        btnNext.style.opacity = 0.5;
+        await connection.invoke("SetCurrentTurn", (+cajaId));
+    }
+
+    setTimeout(() => {
+        btnNext.disabled = false;
+        btnNext.style.opacity = 1;
+        btnNext.style.cursor = "inherit"
+
+    }, 1000)
+});
+
+
+$btnSkipTurno.addEventListener("click", async () => {
+    console.log($turnoActual.dataset.turno)
+
+    if ($turnoActual.dataset.turno) {
+        await connection.invoke("SkipTurn", $turnoActual.dataset.turno);
+    }
+});
+
+
+
+
+
+async function endTurno() {
+
+    const cajaId = localStorage.getItem("cajaId");
+    const cajaNumero = localStorage.getItem("cajaNumero");
+    $btnEndTurno.disabled = true;
+    await connection.invoke("SetInactiveCaja", +cajaId)
+    localStorage.removeItem("cajaId");
+    localStorage.removeItem("cajaNumero");
+    window.location.reload();
+    $btnEndTurno.disabled = false;
+}
+
+function init() {
+    if (cajaId != null && cajaNumero != null) {
+        $containerTurnosControl.style.display = "block";
+        $containerCajasControl.style.display = "none";
+        $caja.style.display = "block";
+        $caja.textContent = `Caja: ${cajaNumero}`;
+    }
+
+    const turnoActual = localStorage.getItem("turnoActual");
+
+    if (turnoActual) {
+        $turnoActual.textContent = `Turno: ${turnoActual}`;
+        $turnoActual.dataset.turno = turnoActual;
+    }
+
+
+}
+
+
+
+// Start the connection.
+init();
+start();
---
+++ QBankAdmin/wwwroot/usuario.js
@@ -0,0 +1,131 @@
+﻿const url = "https://qbank.websitos256.com/turno";
+const urlLocal = "https://localhost:5002/turno";
+const turnos = document.querySelectorAll(".container__turnos-item");
+const currentTurno = document.querySelector("#turnoActual");
+const $siguentesTurnos = document.querySelectorAll(".container__turnos-item-siguientes-turnos")
+
+const connection = new signalR.HubConnectionBuilder()
+    .withUrl(urlLocal, {
+        skipNegotiation: true,
+        transport: signalR.HttpTransportType.WebSockets,
+    })
+    .configureLogging(signalR.LogLevel.Information)
+    .build();
+
+async function start() {
+    try {
+        await connection.start();
+        console.log("SignalR Connected.");
+    } catch (err) {
+        console.log(err);
+        setTimeout(start, 5000);
+    }
+};
+
+connection.onclose(async () => {
+    await start();
+});
+
+
+connection.on("AddToQueue", (turno) => {
+    const turnosContainer = document.querySelector(".container__turnos");
+
+    const $div = document.createElement("div");
+    $div.classList.add("container__turnos-item");
+    const pSiguientesTurnos = document.createElement("p");
+    pSiguientesTurnos.classList.add("container__turnos-item-siguientes-turnos");
+    $div.appendChild(pSiguientesTurnos);
+    const p = document.createElement("p");
+    p.textContent = `${turno.codigoTurno}`;
+    $div.appendChild(p);
+
+    turnosContainer.appendChild($div);
+
+
+});
+
+
+connection.on("SetCurrentTurn", (turno, caja) => {
+
+    if (turno && caja) {
+        const $cajaDeTurnoActual = document.querySelector(".container__turnos-actual-caja");
+        const turnos = document.querySelectorAll(".container__turnos-item");
+        const p = document.createElement("p");
+        currentTurno.innerHTML = "";
+        p.textContent = `${turno}`;
+        currentTurno.appendChild(p);
+        $cajaDeTurnoActual.textContent = `CAJA ${caja}`;
+
+        turnos.forEach((item) => {
+            if (item.textContent.includes(turno)) {
+                item.remove();
+            }
+        });
+
+        localStorage.setItem("turnoActual", turno);
+        localStorage.setItem("cajaActual", caja);
+    }
+});
+
+
+connection.on("SkipTurn", (turno, siguienteTurno) => {
+    if (turno, siguienteTurno) {
+        const $cajaDeTurnoActual = document.querySelector(".container__turnos-actual-caja");
+        const turnos = document.querySelectorAll(".container__turnos-item");
+        currentTurno.textContent = `${siguienteTurno.codigoTurno}`;
+        $cajaDeTurnoActual.textContent = `CAJA ${siguienteTurno.caja}`;
+
+        turnos.forEach((item) => {
+            if (item.textContent.includes(siguienteTurno.codigoTurno)) {
+                item.remove();
+            }
+        });
+
+        const $turnoSaltado = document.createElement("div");
+        $turnoSaltado.classList.add("container__turnos-item");
+        const pSaltado = document.createElement("p");
+        const pSiguientesTurnos = document.createElement("p");
+        pSiguientesTurnos.classList.add("container__turnos-item-siguientes-turnos");
+        $turnoSaltado.appendChild(pSiguientesTurnos);
+        pSaltado.textContent = `${turno.codigoTurno}`;
+        $turnoSaltado.appendChild(pSaltado);
+        document.querySelector(".container__turnos").appendChild($turnoSaltado);
+
+
+        localStorage.setItem("turnoActual", siguienteTurno.codigoTurno);
+        localStorage.setItem("cajaActual", siguienteTurno.caja);
+    }
+
+})
+
+
+function init() {
+    const turnoActual = localStorage.getItem("turnoActual");
+    const cajaActual = localStorage.getItem("cajaActual");
+
+    $siguentesTurnos.forEach((item, index) => {
+        if (index == 0) {
+            item.textContent = "Proximos turnos";
+            return;
+        }
+    });
+
+    if (turnoActual && cajaActual) {
+        const $caja = document.querySelector(".container__turnos-actual-caja");
+        const $turno = document.querySelector("#turnoActual");
+
+        $caja.textContent = `CAJA ${cajaActual}`;
+        $turno.textContent = turnoActual;
+
+
+    }
+
+
+
+
+}
+
+
+// Start the connection.
+init();
+start();
