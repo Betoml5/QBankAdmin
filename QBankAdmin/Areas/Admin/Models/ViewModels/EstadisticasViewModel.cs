fd1cea71aec573342660532f16ebcd275aa56019
RodrigoTLZ <201G0246@rcarbonifera.tecnm.mx>
2024-06-06 23:02:11 -0600

    implementacion de estadisticas en su propia vista

---
+++ QBankAdmin/Areas/Admin/Models/ViewModels/EstadisticasViewModel.cs
@@ -0,0 +1,11 @@
+﻿using QBankAdmin.Models.Dtos;
+
+namespace QBankAdmin.Areas.Admin.Models.ViewModels
+{
+    public class EstadisticasViewModel
+    {
+        public EstadisticaDTO EstadisticaActual { get; set;}
+
+        public IEnumerable<EstadisticaDTO>? HistorialEstadisticas { get; set;}
+    }
+}
--- QBankAdmin/Models/Dtos/EstadisticaDTO.cs
+++ QBankAdmin/Models/Dtos/EstadisticaDTO.cs
@@ -6,7 +6,7 @@
 		public int TurnosAtendidos { get; set; }
 		public int TurnosPendientes { get; set; }
 		public int TurnosEnProgreso { get; set; }
-		public double TiempoPromedioEspera {  get; set; }
+		public double TiempoPromedioDeEspera {  get; set; }
 		public double TiempoPromedioDeAtencion {  get; set; }
     }
 }
